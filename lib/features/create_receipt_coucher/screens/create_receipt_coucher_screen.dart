df036de18e42ad3a34bd3de1edda18c995c82b04
TopBusiness <semicolonsales@gmail.com>
2024-11-12 13:06:43 -0800

    انشاء سند القبض

--- lib/features/create_receipt_coucher/screens/create_receipt_coucher_screen.dart
+++ lib/features/create_receipt_coucher/screens/create_receipt_coucher_screen.dart
@@ -169,6 +169,9 @@ class _CreateReceiptCoucherScreenState
 
               cubit.partnerPaymentMethod(context, partnerId: widget.partnerId);
             },
+
+
+
           )
         ]),
       ),
